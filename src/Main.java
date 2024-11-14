b79e78ada8a75059b211019dd66be1e464409e50
Gabriel <campelinho_1957@outlook.be>
2024-11-14 14:18:03 +0100

    E1_forloop

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+    for ( int i = 36; i<=60 ; i++)
+    {
+        if (i%2==0)
+        {
+            System.out.println(i);
+        }
+    }
+    }
+}
